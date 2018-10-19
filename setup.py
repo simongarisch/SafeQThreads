d533982a97e11f7c708dec8764c2ee6909030e7b
simongarisch <gatman946@gmail.com>
2018-10-19 19:26:27 +1100

    Adding key files: travis, requirements, setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(name="safeqthreads",
+      version="0.0.1",
+      install_requires=[
+          "QtPy>=1.5.0"
+        ],
+      description="Track and manage your QThreads.",
+      long_description=open("README.md").read(),
+
+      author="Simon Garisch",
+      author_email="gatman946@gmail.com",
+      url="https://github.com/simongarisch/safeqthreads",
+      packages=find_packages()
+     )
